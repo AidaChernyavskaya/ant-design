c48f5d840d4ee718a52f7f9a9c6521f7bd0c4c92
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2024-05-07 16:39:06 +0300

    Try Flex component

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,27 @@ import {HeartOutlined} from "@ant-design/icons";
 import { FloatButton } from "antd";
 import {Typography} from "antd";
 
+import {Divider} from "antd";
+import {Flex} from "antd";
+
 function App() {
     const [editableStr, setEditableStr] = useState('Simple text');
   return (
       <div>
-          <Button type={"primary"} style={{ background: "coral"}}>Primary button</Button>
-          <Button type={"default"} icon={<HeartOutlined/>}>Default button</Button>
-          <Button type={"dashed"} icon={<HeartOutlined/>} iconPosition={'end'}>Dashed button</Button>
-          <Button type={"text"}>Text button</Button>
-          <Button type={"link"} loading={true} danger={true}>Link button</Button>
+          <Flex vertical={true} justify={"flex-start"} align={"flex-start"} gap={10}>
+              <Button type={"primary"} style={{ background: "coral"}}>Primary button</Button>
+              <Button type={"default"} icon={<HeartOutlined/>}>Default button</Button>
+              <Button type={"dashed"} icon={<HeartOutlined/>} iconPosition={'end'}>Dashed button</Button>
+              <Button type={"text"}>Text button</Button>
+              <Button type={"link"} loading={true} danger={true}>Link button</Button>
+          </Flex>
 
           <FloatButton type={'primary'} tooltip={'Help'} badge={{count: 5, color: 'coral'}}/>
 
           <Typography.Text editable={{onChange: setEditableStr}}>{editableStr}</Typography.Text>
           <Typography.Title level={1}>Header 1</Typography.Title>
+
+          <Divider children={'Divider text'}/>
       </div>
 
   );
