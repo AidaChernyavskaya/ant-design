1ee851a3e45724d00961b3327b9d5e4de07308a6
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2024-05-13 19:53:26 +0300

    Add Typography antd elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Button, FloatButton, Typography} from "antd";
+import {Button, FloatButton, Input, Typography} from "antd";
 import { HeartOutlined} from "@ant-design/icons";
 
 import {Divider, Flex, Layout} from "antd";
@@ -20,11 +20,39 @@ function App() {
 
           <Content style={{padding: '50px'}}>
               <Breadcrumb
-                  items={[{title: 'Home'}, {title: <a href={''}>Layout</a>}, {title: 'Classic Layout'}]}
+                  items={[{title: 'Home'}, {title: <a href={''}>All articles</a>}, {title: 'Article name'}]}
                   style={{marginBottom: '20px'}}
               />
-              <div style={{background: 'white', padding: '50px 10px'}}>
-                  <Flex vertical={true} justify={"flex-start"} align={"flex-start"} gap={10}>
+              <div style={{background: 'white', padding: '30px 10px'}}>
+                  <Typography.Title level={2}>Article header</Typography.Title>
+                  <Typography.Title level={4}>Short article description</Typography.Title>
+                  <Divider/>
+                  <Typography.Paragraph ellipsis={{rows:3, expandable: 'collapsible'}}>
+                      {'Arcu ut. Sed orci, efficitur vestibulum lectus amet eleifend quis, tortor, odio.'.repeat(30)}
+                  </Typography.Paragraph>
+                  <Divider/>
+
+
+                  <Input placeholder={'Type your comment'} style={{marginBottom: '10px'}}/>
+                  <Button type={"primary"} style={{ background: "coral"}}>Leave comment</Button>
+                  <Divider/>
+
+                  <Typography.Title level={5}>3 comments</Typography.Title>
+                  <Flex vertical={true} justify={"flex-start"} align={"flex-start"} style={{marginBottom: '10px'}}>
+                      <Typography.Text strong={true}>User Name</Typography.Text>
+                      <Typography.Text>{'Comment text'.repeat(5)}</Typography.Text>
+                  </Flex>
+                  <Flex vertical={true} justify={"flex-start"} align={"flex-start"} style={{marginBottom: '10px'}}>
+                      <Typography.Text strong={true}>User Name 2</Typography.Text>
+                      <Typography.Text>{'Comment text 22'.repeat(5)}</Typography.Text>
+                  </Flex>
+                  <Flex vertical={true} justify={"flex-start"} align={"flex-start"} style={{marginBottom: '10px'}}>
+                      <Typography.Text strong={true}>User Name 3</Typography.Text>
+                      <Typography.Text>{'Comment text 333'.repeat(5)}</Typography.Text>
+                  </Flex>
+
+
+                  <Flex vertical={true} justify={"flex-start"} align={"flex-start"} gap={10} style={{marginTop: '40px'}}>
                       <Button type={"primary"} style={{ background: "coral"}}>Primary button</Button>
                       <Button type={"default"} icon={<HeartOutlined/>}>Default button</Button>
                       <Button type={"dashed"} icon={<HeartOutlined/>} iconPosition={'end'}>Dashed button</Button>
@@ -35,9 +63,6 @@ function App() {
                   <FloatButton type={'primary'} tooltip={'Help'} badge={{count: 5, color: 'coral'}}/>
 
                   <Typography.Text editable={{onChange: setEditableStr}}>{editableStr}</Typography.Text>
-                  <Typography.Title level={1}>Header 1</Typography.Title>
-
-                  <Divider children={'Divider text'}/>
               </div>
           </Content>
 
